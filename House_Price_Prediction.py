351da1a1019e6a1d0e56f1eea7f88f28a2b947c8
Mayank Mishra <mayank1996.skb@hotmail.com>
2017-09-16 10:14:29 +0530

    Update House_Price_Prediction.py

--- House_Price_Prediction.py
+++ House_Price_Prediction.py
@@ -111,6 +111,6 @@ with tf.Session() as sess:
     plt.plot(test_house_size,test_house_price,"mo",label = "Testing data")
     plt.plot(train_house_size_norm * train_house_size_std + train_house_size_mean,
     (sess.run(tf_size_factor) * train_house_size_norm + sess.run(tf_price_offset)) * train_price_std + train_price_mean,
-    label = "Learner=d Regression")
+    label = "Learned Regression")
     plt.legend(loc = "upper left")
     plt.show()
