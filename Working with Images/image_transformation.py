c7fb6160e223d00975573f6324574808506b853f
mayankskb <mayank1996.skb@hotmail.com>
2018-03-12 23:52:52 +0530

    List of Images 4-D Tensors

---
+++ Working with Images/image_transformation.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+from PIL import Image
+
+original_image_list = ['images/black_woods.jpg',
+                        'images/cute-white-puppy.jpg',
+                        'images/indian_actor.jpg',
+                        'images/old_ruins.jpeg']
+
+
+# Make a queue of file names including all the images specified--- Queues construct in tensor flow to read the image files
+filename_queue = tf.train.string_input_producer(original_image_list)
+
+
+# Read an entire image file
+image_reader = tf.WholeFileReader()
+
+# session object in tensor flow is a multi threaded session
+with tf.Session() as sess:
+    # Coordinate the loading of image files. This makes working with threads and queues very very easy
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess = sess, coord = coord)
+
+    image_list = []
+    for i in range(len(original_image_list)):
+        # Read a whole file from the queue, the first returned value in the tuple is the
+        # filename which we are ignoring.
+        _, image_file = image_reader.read(filename_queue)
+
+        # Decode the image file as a JPEG file, this will turn it into a Tensor which we can
+        # then use in training
+        image = tf.image.decode_jpeg(image_file)
+
+        # Get a tensor of resize image
+        image = tf.image.resize_images(image, [300, 300])
+        image.set_shape((300,300,3))
+
+        #Get an image tenso and print its value
+        image_array = sess.run(image)
+        print(image_array.shape)
+
+        #using pillow library function to show the image
+        Image.fromarray(image_array.astype('uint8'), 'RGB').show()
+
+        # The expand_dims method add a new dimension
+        image_list.append(tf.expand_dims(image_array, 0))
+
+    # Finishes off the filename queue Coordinator
+    coord.request_stop()
+    coord.join(threads)
+
+    index = 0
+
+
+    # Writes image summary
+    summary_writer = tf.summary.FileWriter('./image_resize', graph = sess.graph)
+
+    for image_tensor in image_list:
+         summary_str = sess.run(tf.summary.image('image -'+str(index), image_tensor))
+         summary_writer.add_summary(summary_str)
+         index += 1
+
+    summary_writer.close()
