bf4fdfaaca64b2c8d086151b7376dcfbde405a64
mayankskb <mayank1996.skb@hotmail.com>
2018-03-20 19:08:28 +0530

    Stepest descent regression

--- TensorFlow_Regression/OnePointPerEpoch.py
+++ TensorFlow_Regression/OnePointPerEpoch.py
@@ -24,7 +24,7 @@ print(googModel.intercept_)
 import tensorflow as tf
 
 # Model linear regression y = Wx + b
-W = tf.Variable(tf.zeros(1,1))
+W = tf.Variable(tf.zeros([1,1]))
 b = tf.Variable(tf.zeros([1]))
 
 # Placeholder to feed in the returns, returns have many rows,
@@ -34,11 +34,60 @@ x = tf.placeholder(tf.float32, [None, 1])
 Wx = tf.matmul(x, W)
 y = Wx + b
 
+# Add summary ops to collect data
+W_hist = tf.summary.histogram('weights', W)
+b_hist = tf.summary.histogram('biases', b)
+y_hist = tf.summary.histogram('y', y)
+
 # Placeholder to hold the y-labels, also returns
 y_ = tf.placeholder(tf.float32, [None, 1])
 
 # Cost function
 cost = tf.reduce_mean(tf.square(y_ - y))
 
+cost_hist = tf.summary.histogram('cost', cost)
+
 # Initializing the optimizer
 train_step_constant = tf.train.GradientDescentOptimizer(0.1).minimize(cost)
+
+
+# Set up a method to perform the actual training. Allow us to
+# modify the optimizer used and also the number of steps
+# in the training
+def trainWithOnePointPerEpoch(steps, train_step):
+
+	init = tf.global_variables_initializer()
+
+	with tf.Session() as sess:
+		sess.run(init)
+
+		merged_summary = tf.summary.merge_all()
+		writer = tf.summary.FileWriter('./OnePointPerEpoch_logs', sess.graph)
+
+		for i in range(steps):
+
+			# Extract one training point
+			xs = np.array([[xData[i % len(xData)]]])
+			ys = np.array([[yData[i % len(yData)]]])
+
+			feed = { x: xs, y_: ys}
+
+			sess.run(train_step, feed_dict = feed)
+
+			# Write out histogram summaries
+			result = sess.run(merged_summary, feed_dict = feed)
+			writer.add_summary(result, i)
+
+			# Print result to screen for every 1000 iteration
+			if(i + 1) % 1000 == 0:
+				print('After %d iteration:' % i)
+
+				print('W: %f' % sess.run(W))
+				print('b: %f' % sess.run(b))
+
+				print('cost: %f' % sess.run(cost, feed_dict = feed))
+
+			writer.close()
+
+
+trainWithOnePointPerEpoch(10000, train_step_constant)
